d63e74dafd7f142c4719b910d8b1ab2336c56d03
Kunsh <k789.tyagi@gmail.com>
2026-01-29 22:25:25 +0530

    router setup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routes.admin.upload import router as upload_router
-from routes import router as predict_router
+#from routes.predict import router as predict_router
 
 app = FastAPI()
 print("server running ")
@@ -10,4 +10,4 @@ def root():
     return {"body": "Welcome to CCS Smart Drive"}
 
 app.include_router(upload_router)
-app.include_router(predict_router)
+#app.include_router(predict_router)
