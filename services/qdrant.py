9517d5b05f7def7a2b00f69639da193ede4811d6
Kunsh <k789.tyagi@gmail.com>
2026-01-30 02:02:44 +0530

    Qdrant Env setup

--- services/qdrant.py
+++ services/qdrant.py
@@ -1,6 +1,9 @@
 from qdrant_client import QdrantClient
+from config import QDRANT_API_KEY, QDRANT_API_URL
 
 qdrant_client = QdrantClient(
+    url=QDRANT_API_URL, 
+    api_key=QDRANT_API_KEY,
 )
 
 print(qdrant_client.get_collections())
