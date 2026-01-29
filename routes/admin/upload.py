2b0527ffe4a536c98a986c8822a9f3db91e8c86d
Kunsh <k789.tyagi@gmail.com>
2026-01-30 00:11:10 +0530

    Api Key and File upload

--- routes/admin/upload.py
+++ routes/admin/upload.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, UploadFile, File, Form, Header
 from pydantic import BaseModel
 import config
 
 router = APIRouter(prefix="/admin")
+API_KEY = config.API_KEY
 
-@router.get('/upload')
-def upload_image():
-    return "upload request"
+@router.post('/upload')
+def upload_image(
+    image: UploadFile=File(...),
+    event: str = Form(...),
+    x_api_key: str = Header(None)):
+
+    if x_api_key==API_KEY:
+        return {
+            "filename": image.filename,
+            "content_type": image.content_type,
+            "event": event
+        }
+    else:
+        return "Unauthorized Access"
